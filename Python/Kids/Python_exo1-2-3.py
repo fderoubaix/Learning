b24021bad606ba0b9bf80908c13ca0f09370040c
Deroubaix Frederic <frederic.Deroubaix@fullsix.com>
2016-11-11 21:38:53 +0100

    Premier commit

---
+++ Python/Kids/Python_exo1-2-3.py
@@ -0,0 +1,16 @@
+#Exo1 : les tableau 
+jeux=["Playstation","PC", "JeuxVideo"]
+nourritures=["Pizza","Pattes","Raclette"]
+favoris=jeux+nourritures
+print(favoris)
+
+#Exo2 : Les calculs simples
+nombre=25*3+2*40
+print(nombre)
+
+#Exo3 : Affichage d'un texte avec des variables
+
+Message="Bonjour, %s %s !."
+Prenom="Frederic"
+Nom="Deroubaix"
+print(Message % (Prenom, Nom))
